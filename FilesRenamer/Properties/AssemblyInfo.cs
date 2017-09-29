026ba4e247494ab139b9a58e60548dce86e4f8dc
paolopasqua <paolo.pasquali@outlook.it>
2017-09-29 17:50:47 +0200

    Updates to assemby info

--- FilesRenamer/Properties/AssemblyInfo.cs
+++ FilesRenamer/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Le informazioni generali relative a un assembly sono controllate dal seguente 
 // set di attributi. Modificare i valori di questi attributi per modificare le informazioni
 // associate a un assembly.
-[assembly: AssemblyTitle("RinominaFiles")]
+[assembly: AssemblyTitle("FilesRenamer")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("RinominaFiles")]
+[assembly: AssemblyCompany("PaoloPasquali")]
+[assembly: AssemblyProduct("FilesRenamer")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
